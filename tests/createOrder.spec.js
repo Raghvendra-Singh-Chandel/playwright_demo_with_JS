6b3a8e93bc894a95422206c0f403b2558b998c4e
= <=>
2023-08-03 10:41:41 +0530

    modified viewport

--- tests/createOrder.spec.js
+++ tests/createOrder.spec.js
@@ -1,12 +1,12 @@
 const {test} = require("./Support/Fixture/pageObjectFixture");
 
-test.describe("verify login page functionality",()=> {
+test.describe("createOrder end to end scenario",()=> {
     test.beforeEach(async({loginPage})=> {
         await loginPage.landingToLoginPage()
         await loginPage.loginWitValidCredential()
     })
 
-    test("verify functionality of login with invalid credential", async({selectProduct, personDetails, paymentDetails})=> {
+    test("create order", async({selectProduct, personDetails, paymentDetails})=> {
         await selectProduct.selectproductToBuy()
         await selectProduct.placeOrder()
         await personDetails.fillAlldetailsAndProcessedToPaymentPage()
